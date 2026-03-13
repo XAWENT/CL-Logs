4b936957d6f2610c4a52349597c1eac60a1e871d
pichotiner <m3cromz@gmail.com>
2026-03-13 21:38:50 +0300

    sadasd

--- log_parser/parsers/elasticsearch_parser.py
+++ log_parser/parsers/elasticsearch_parser.py
@@ -9,7 +9,6 @@ from typing import Dict, Any
 class ElasticsearchParser:
     """Parser for Elasticsearch logs."""
     
-    # Main ES format: [2024-01-15T10:30:45,123][INFO ][class] message
     _pattern = re.compile(
         r"^\[(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:,\d{3})?)\]"
         r"\[(\w+)\]"
